c49bb86a670e34f7213325dc719123bef707fce8
uturkmen16 <umutcan.turkmen@hotmail.com>
2023-05-02 03:36:32 +0300

    added spin functionality and slot elements to bronze prefab

--- Assets/Scripts/LevelSettings.cs
+++ Assets/Scripts/LevelSettings.cs
@@ -6,10 +6,29 @@ using System;
 [CreateAssetMenu(menuName = "SpinWheel/LevelSettings")]
 public class LevelSettings : ScriptableObject
 {
+    public void ShuffleRewards() {
+        int len = _rewards.Length;
+        for (int i = 0; i < len; i++) {
+            int randomIndex = i + UnityEngine.Random.Range(0, len - i);
+            SlotValue temp = _rewards[randomIndex];
+            _rewards[randomIndex] = _rewards[i];
+            _rewards[i] = temp;
+        }
+    }
     [Serializable]
     public struct SlotValue {
-        public ItemType itemtype;
-        public int amount;
+        public ItemType itemType;
+        public int amount;    
+        
+        public SlotValue(ItemType itemType, int amount) {
+            this.itemType = itemType;
+            this.amount = amount;
+        }
+    
+        public SlotValue(SlotValue other) {
+            this.itemType = other.itemType;
+            this.amount = other.amount;
+        }
     }
 
     const int size = 8;
@@ -23,11 +42,15 @@ public class LevelSettings : ScriptableObject
 
     [SerializeField] private SlotValue[] _rewards = new SlotValue[size];
 
-    public SlotValue[] Rewards { get {return _rewards;}}
+    public SlotValue[] Rewards { 
+        get {
+            return _rewards;
+        }
+    }
+
     
 
-    void OnValidate()
-    {
+    void OnValidate() {
         if (_rewards.Length != size) {
             Debug.LogWarning("Don't change the rewards array size!");
             Array.Resize(ref _rewards, size);
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,20 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField]
     List<LevelSettings> levelSettings;
     // Start is called before the first frame update
-    void Start()
-    {
-        GameObject instance = Instantiate(levelSettings[0].Prefab, this.transform);
-    }
+    void Start() {
+        //Bronze - 0, Silver - 1, Gold - 2
+        int levelType = 0;
 
+        levelSettings[levelType].ShuffleRewards();
+
+        GameObject instance = Instantiate(levelSettings[levelType].Prefab, transform);
+        Transform baseTransform = instance.transform.GetChild(0);
+
+        for(int i = 0; i < baseTransform.childCount; i++) {
+            Transform childTransform = baseTransform.GetChild(i);
+            childTransform.GetComponentInChildren<Image>().sprite = levelSettings[levelType].Rewards[i].itemType.itemIcon;
+            int amount = levelSettings[levelType].Rewards[i].amount;
+            string suffix = "";
+            if(amount > 999999) {
+                //Million
+                amount /= 1000000;
+                suffix = "M";
+            }
+            else if(amount > 999) {
+                //Thousand
+                amount /= 1000;
+                suffix = "K";
+            }
+            childTransform.GetComponentInChildren<TextMeshProUGUI>().text = "x" + amount.ToString() + suffix;
+        }
+    }
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
